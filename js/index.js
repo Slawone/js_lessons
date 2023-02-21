0bedb999c4ce33b7b9de3a46fcb8d7875d07965a
Slawone <for1s8@ya.ru>
2023-02-21 18:33:49 +0300

    fix tasksHard_1

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-const productName = prompt('Введите название товара');
-const productSum = prompt('Введите количество товара');
-const productCategory = prompt('Назовите категорию товара');
-const productPrice = prompt('Укажите стоимость одной единицы товара');
+// const productName = prompt('Введите название товара');
+// const productSum = prompt('Введите количество товара');
+// const productCategory = prompt('Назовите категорию товара');
+// const productPrice = prompt('Укажите стоимость одной единицы товара');
 
-if (isFinite(productSum) && isFinite(productPrice)) {
-  console.log(`На складе ${productSum} единицы товара "${productName}" на сумму ${productSum * productPrice} деревянных.`);
-} else {
-  console.log('Вы ввели некорректные данные');
-}
+// if (isFinite(productSum) && isFinite(productPrice)) {
+//   console.log(`На складе ${productSum} единицы товара "${productName}" на сумму ${productSum * productPrice} деревянных.`);
+// } else {
+//   console.log('Вы ввели некорректные данные');
+// }
--- js/tasksHard_1.js
+++ js/tasksHard_1.js
@@ -1,19 +1,18 @@
 const income = +prompt('Введите ваш доход, чтобы узнать стандартный налог');
+let tax;
 
 if (income < 15000) {
-  const tax = income * 0.13;
-  const total = income - tax;
-  console.log(`Ваш налог составил ${tax} руб. к выдаче ${total} руб.`);
+  tax = income * 0.13;
 }
 
 if (income >= 15000 && income <= 50000) {
-  const tax = income * 0.2;
-  const total = income - tax;
-  console.log(`Ваш налог составил ${tax} руб. к выдаче ${total} руб.`);
+  tax = income * 0.2;
 }
 
 if (income > 50000) {
-  const tax = income * 0.3;
-  const total = income - tax;
-  console.log(`Ваш налог составил ${tax} руб. к выдаче ${total} руб.`);
+  tax = income * 0.3;
 }
+
+const total = income - tax;
+
+console.log(`Ваш налог составил ${tax} руб. к выдаче ${total} руб.`);
