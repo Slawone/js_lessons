3c0858e6be9aad9297187541229b87745262ea8b
Slawone <for1s8@ya.ru>
2023-02-21 23:24:10 +0300

    fix tasksHard_2

--- js/tasksHard_1.js
+++ js/tasksHard_1.js
@@ -1,18 +1,18 @@
-const income = +prompt('Введите ваш доход, чтобы узнать стандартный налог');
-let tax;
+// const income = +prompt('Введите ваш доход, чтобы узнать стандартный налог');
+// let tax;
 
-if (income < 15000) {
-  tax = income * 0.13;
-}
+// if (income < 15000) {
+//   tax = income * 0.13;
+// }
 
-if (income >= 15000 && income <= 50000) {
-  tax = income * 0.2;
-}
+// if (income >= 15000 && income <= 50000) {
+//   tax = income * 0.2;
+// }
 
-if (income > 50000) {
-  tax = income * 0.3;
-}
+// if (income > 50000) {
+//   tax = income * 0.3;
+// }
 
-const total = income - tax;
+// const total = income - tax;
 
-console.log(`Ваш налог составил ${tax} руб. к выдаче ${total} руб.`);
+// console.log(`Ваш налог составил ${tax} руб. к выдаче ${total} руб.`);
