5c64d3fc1bc3af890222948e47a36b5793dc8d8f
Slawone <for1s8@ya.ru>
2023-02-22 17:58:35 +0300

    fix hard_task_02

--- js/hard_task_02.js
+++ js/hard_task_02.js
@@ -1,8 +1,8 @@
 const num1 = +prompt('Введите число 1');
 const num2 = +prompt('Введите число 2');
 
-const minNumnum2er = (num1, num2) => {
+const minNumber = (num1, num2) => {
   return (num1 > num2) * num2 + (num2 > num1) * num1;
 }
 
-console.log(`Минимальное число ${minNumnum2er(num1, num2)}`);
+console.log(`Минимальное число ${minNumber(num1, num2)}`);
