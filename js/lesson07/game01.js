2585385340a24a3f1a4be7d9c1ccb8e24e6df0c2
Slawone <for1s8@ya.ru>
2023-02-28 22:43:06 +0300

    update game01

--- js/lesson07/game01.js
+++ js/lesson07/game01.js
@@ -1,1 +1,20 @@
+const randomNumber = Math.round(Math.random() * 100);
+console.log(randomNumber);
 
+let input;
+
+while (input = prompt('Введите число от 1 до 100', '')) {  
+  if (isNaN(input)) {
+    alert('Введите число')
+  }
+  if (input > randomNumber) {
+    alert("Угадываемое число меньше!");
+  }
+  if (input < randomNumber) {
+    alert("Угадываемое число больше!");
+  }
+  if (input == randomNumber) {
+    alert("Правильно! Вы выиграли!");
+    break;
+  }
+}
