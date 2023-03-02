eec3dcab4231a04c0258df75a75b444555f3ae64
Slawone <for1s8@ya.ru>
2023-03-02 19:53:27 +0300

    module 4 lesson 8

---
+++ js/lesson08/cart.js
@@ -0,0 +1,44 @@
+const cart = {
+  items: [],
+  totalPrice: 0,
+  count: 0,
+  getTotalPrice: function() {
+    return this.totalPrice;
+  },
+  add: function(product, price, productCount = 1) {
+    this.items.push({
+      product,
+      price,
+      productCount,
+    })
+    this.calculateItemPrice(price, productCount)
+    this.increaseCount(productCount);
+  },
+  increaseCount: function(num) {
+    return this.count += num;
+  },
+  calculateItemPrice: function(price, productCount) {
+    this.totalPrice += price * productCount;
+  },
+  clear: function() {
+    const newCart = {
+      items: [],
+      totalPrice: 0,
+      count: 0,
+    };
+    Object.assign(this, newCart);
+  },
+  print: function() {
+    const jsonItems = JSON.stringify(this.items);
+    const price = this.getTotalPrice();
+    console.log(jsonItems);
+    console.log(price);
+  },
+};
+
+cart.add('beer', 79, 6);
+cart.add('fish', 115, 4);
+cart.add('cola', 145, 2);
+cart.add('package', 7, 1);
+
+cart.print();
